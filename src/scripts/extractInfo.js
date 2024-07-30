ab78f856aefe8eb8356877577ea5b00088d5a70f
joaquinvesapa <joaquinvesapa@gmail.com>
2024-07-30 09:57:26 -0300

    Add Hour to ExtractInfo script

--- src/scripts/extractInfo.js
+++ src/scripts/extractInfo.js
@@ -14,13 +14,19 @@ function convertDate(dateString) {
 }
 const partidos = await page.$$eval('.Table__fixtures', (results) =>
 	results.map((table) => {
+		//Extract mont
 		const month = table.querySelector('.Table__Title')?.innerText
 
 		let matches = []
-
+		//Extract year
 		const year = month.match(/\d{4}/)
 
+		//Extract match date
 		table.querySelectorAll('.Table__TBODY tr').forEach((match) => {
+			let dateString = new Date(
+				match.querySelector('.matchTeams')?.innerText + ` ${year}`
+			).toLocaleString()
+
 			let date = new Date(
 				match.querySelector('.matchTeams')?.innerText + ` ${year}`
 			).toLocaleDateString('es-ES', {
@@ -30,26 +36,32 @@ const partidos = await page.$$eval('.Table__fixtures', (results) =>
 				day: 'numeric',
 			})
 
+			//Format date
+			date = date.charAt(0).toUpperCase() + date.slice(1)
+
+			//Extract versus
 			const versus =
 				match.querySelector('.away').innerText === 'Argentina'
 					? match.querySelector('.local').innerText
 					: match.querySelector('.away').innerText
 
-			date = date.charAt(0).toUpperCase() + date.slice(1)
+			const hour = match.querySelector('.Table__TD:nth-child(5) a').innerText
 
+			//Extract competition name
 			const competition = match.querySelector(
 				'.Table__TD:nth-child(6) span'
 			).innerText
-			matches.push({ date, versus, competition })
+
+			matches.push({ date, dateString, hour, versus, competition })
 		})
 
 		return { month, matches }
 	})
 )
 
-// console.log(
-// 	util.inspect(partidos, { showHidden: false, depth: null, colors: true })
-// )
+console.log(
+	util.inspect(partidos, { showHidden: false, depth: null, colors: true })
+)
 fs.writeFileSync('../../public/matches.json', JSON.stringify(partidos), 'utf8')
 
 console.log('JSON file has been written successfully.')
