6748f1f402a810d96d4835d8fc95bdb1d4314c87
joaquinvesapa <joaquinvesapa@gmail.com>
2024-08-28 11:11:16 -0300

    chore: Update vercel web analytics configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import vercel from '@astrojs/vercel/serverless'
 export default defineConfig({
 	integrations: [tailwind()],
 	output: 'server',
-	adapter: vercel({ webAnalytics: true }),
+	adapter: vercel({ webAnalytics: { enabled: true } }),
 	plugins: [
 		VitePWA({
 			registerType: 'autoUpdate',
