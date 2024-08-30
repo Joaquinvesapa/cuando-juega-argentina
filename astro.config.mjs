80b5ac15f5a6e4502c72f1497f892f83ceb93165
joaquinvesapa <joaquinvesapa@gmail.com>
2024-08-30 13:59:47 -0300

    chore: Update Vercel configuration and remove unused test file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { manifest } from './src/utils/seoConfig';
 import { VitePWA } from 'vite-plugin-pwa';
 import vercel from '@astrojs/vercel/serverless';
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,37 +12,42 @@ export default defineConfig({
   output: 'server',
   adapter: vercel({
     webAnalytics: {
-      enabled: true
-    }
+      enabled: true,
+    },
   }),
-  plugins: [VitePWA({
-    registerType: 'autoUpdate',
-    manifest,
-    workbox: {
-      globDirectory: '.vercel/output/static',
-      globPatterns: ['**/*.{html,js,css,woff,woff2,ttf,eot,ico}'],
-      runtimeCaching: [{
-        urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp|avif)$/,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'images',
-          expiration: {
-            maxEntries: 100,
-            maxAgeSeconds: 30 * 24 * 60 * 60
-          }
-        }
-      }, {
-        urlPattern: /\.(?:woff|woff2|ttf|eot|ico)$/,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'fonts',
-          expiration: {
-            maxEntries: 10,
-            maxAgeSeconds: 30 * 24 * 60 * 60
-          }
-        }
-      }],
-      navigateFallback: null
-    }
-  })]
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest,
+      workbox: {
+        globDirectory: '.vercel/output/static',
+        globPatterns: ['**/*.{html,js,css,woff,woff2,ttf,eot,ico}'],
+        runtimeCaching: [
+          {
+            urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp|avif)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'images',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+              },
+            },
+          },
+          {
+            urlPattern: /\.(?:woff|woff2|ttf|eot|ico)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'fonts',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+              },
+            },
+          },
+        ],
+        navigateFallback: null,
+      },
+    }),
+  ],
 });
